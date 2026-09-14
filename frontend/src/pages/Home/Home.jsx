e5af34275b932844384c7545db6df4466d3b172b
Emilio balcaza <balcazaemilio1@gmail.com>
2026-09-14 17:59:15 -0300

    agrego footer

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import './Home.css'
 import element2 from '../../assets/logos/logoHojaRecorte2.webp'
 import element3 from '../../assets/logos/logoEcoPulse1.webp'
+import Footer from '../../components/Footer/Footer'
 
 export default function Home() {
     return (
@@ -19,7 +20,7 @@ export default function Home() {
                     <h1 id="tituloPanelIzq">EcoEmisiones</h1>
                     <img id="logoPanelIzq" src={element2} alt="" />
                     <p id="pPanelIzq">
-                        Hace el calculo de cuanto estas aportando a la huella de carbono en tu hogar, empresa, grupo, o evento?... Otros textos
+                        Hace el calculo de cuanto estas aportando a la huella de carbono en tu hogar, empresa, grupo.
                           </p>
                 </div>
 
@@ -36,20 +37,36 @@ export default function Home() {
                     <p id="pPanelDer">
                        Crea tu cuenta, donde podras agregar tus domicilios, vehiculos o grupos de usuarios para generar reportes que te daran los resultados del total de emisiones de dioxido de carbono enviados al ambiente.
                        </p>
+
+                       <p id="ultimoP">
+                    
+                            <li>El equipo de desarrollo</li>
+                            <li>Inicio</li>
+                            <li>Compras</li>
+                            <li>Otro depto</li>
+                          
+                        </p>
                 </div>
 
              </div>
 
             <div className='contenedorBotoneHome '>
+                        <label className='labelFormHome'>Bienvenido, selecciona una opción.</label>
                         <button className=' btn1'>Ingresar</button>
+                        <label className='labelFormHome'>¿Aun no tienes cuenta?.</label>
                         <button className=' btn1'>Registrarse</button>
-                    </div>
+            </div>
+       
 
-            
-                
-
+      
+      
         </div>
+
+       
+           
    </div>
+   
+
     );
 
 }
